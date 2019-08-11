77c8614511b33bf8dc5b1e1f6f06f9020d182aca
naka_t <naka.t06+atlassian@gmail.com>
2019-08-11 16:31:52 +0900

    Couple GP-HSMMを実装．バグがまだあるかも．

--- CGPSegmentation.py
+++ CGPSegmentation.py
@@ -11,12 +11,13 @@ import sys
 import os
 
 
-class GPSegmentation():
+class CGPSegmentation():
     # parameters
-    MAX_LEN = 20
+    MAX_LEN = 13
     MIN_LEN = 5
-    AVE_LEN = 10
+    AVE_LEN = 7
     SKIP_LEN = 1
+    LAG_SIG = 1     # 時間差の分散
 
     def __init__(self, dim, nclass):
         self.dim = dim
@@ -31,7 +32,14 @@ class GPSegmentation():
         self.trans_prob_eos = np.ones( nclass )
         self.is_initialized = False
 
-        self.prior_table = [self.AVE_LEN**i * math.exp(-self.AVE_LEN) / math.factorial(i) for i in range(self.MAX_LEN)]
+        
+        # 時間遅れに関する変数
+        self.lag_range = [-4,-3,-2,-1,0,1,2,3,4]    # ズレの推定範囲
+        self.lag_probs = [ math.exp(-d*d/(2*self.LAG_SIG*self.LAG_SIG)) for d in self.lag_range ]  # ズレの確率
+        self.t_lag_pairs = [] # 時刻tの自身の行動と対応する相手の行動の時間差
+        self.interrel_prob = np.zeros((nclass, nclass)) # 行動の相互関係を表現する確率
+        self.opponent_gp = None  # 相手のGP
+
         
     def load_data(self, filenames, classfile=None ):
         self.data = []
@@ -65,7 +73,9 @@ class GPSegmentation():
 
         # 遷移確率更新
         self.calc_trans_prob()
+        self.own_classes = self.get_classes_list()
 
+        self.t_lag_pairs = [ [] for i in range(len(self.data)) ]
 
 
 
@@ -88,6 +98,11 @@ class GPSegmentation():
         for s in self.segm_in_class[c]:
             datay += [ y for y in s ]
             datax += range(len(s))
+            
+        # 相手のデータも加えて学習
+        for s in self.opponent_gp.segm_in_class[c]:
+            datay += [ y for y in s ]
+            datax += range(len(s)) 
 
         self.gps[c].learn( datax, datay )
 
@@ -129,6 +144,16 @@ class GPSegmentation():
                         plt.plot( range(len(data[:,d])), data[:,d], "o-" )
                     plt.ylim( -1, 1 )
                 plt.savefig( basename+"class%03d_dim%03d.png" % (c, d) )
+                
+        # 相互関係確率を保存
+        np.save( basename + "interrelationship_prob.npy", self.interrel_prob )
+        
+        # 時間差を保存
+        for i in range(len(self.data)):
+            lags = np.zeros( len(self.data[i]) )
+            for t, lag in self.t_lag_pairs[i]:
+                lags[t] = lag
+            np.savetxt( basename+"lag%03d.txt"%i, lags, fmt="%d" )
 
         # テキストでも保存
         np.save( basename + "trans.npy" , self.trans_prob  )
@@ -139,28 +164,36 @@ class GPSegmentation():
             np.save( basename+"class%03d.npy" % c, self.segm_in_class[c] )
 
 
-    def forward_filtering(self, d ):
+    def forward_filtering(self, idx ):
+        d = self.data[idx]
         T = len(d)
-        a = np.zeros( (len(d), self.MAX_LEN, self.numclass) )   # 前向き確率
+        a = np.zeros( (len(d), self.MAX_LEN, self.numclass, len(self.lag_range)) )   # 前向き確率
         z = np.ones( T ) # 正規化定数
+        
+        # 相手のクラスを取得
+        oppnect_classes = self.opponent_gp.get_classes_list()[idx]
 
         for t in range(T):
             for k in range(self.MIN_LEN,self.MAX_LEN,self.SKIP_LEN):
                 if t-k<0:
                     break
+            
 
                 segm = d[t-k:t+1]
                 for c in range(self.numclass):
                     out_prob = self.calc_emission_prob( c, segm )
                     foward_prob = 0.0
+                    
 
                     # 遷移確率
                     tt = t-k-1
                     if tt>=0:
                         #for kk in range(self.MAX_LEN):
                         #    for cc in range(self.numclass):
-                        #        foward_prob += a[tt,kk,cc] * self.trans_prob[cc, c]
-                        foward_prob = np.sum( a[tt,:,:] * self.trans_prob[:,c] ) * out_prob * z[tt]
+                        #        for ll in range(len(self.lag_range)):
+                        #            foward_prob += a[tt,kk,cc,ll] * self.trans_prob[cc, c] * z[tt]
+                        # t-k-1を終端とし多分節の可能性をすべて周辺化
+                        foward_prob = np.sum( np.sum(a[tt,:,:,:],2) * self.trans_prob[:,c] ) * out_prob * z[tt]
                     else:
                         # 最初の単語
                         foward_prob = out_prob * self.trans_prob_bos[c]
@@ -168,16 +201,23 @@ class GPSegmentation():
                     if t==T-1:
                         # 最後の単語
                         foward_prob *= self.trans_prob_eos[c]
-
-                    # 正規化を元に戻す
-                    a[t,k,c] = foward_prob
-                    if math.isnan(foward_prob):
-                        print( "a[t=%d,k=%d,c=%d] became NAN!!" % (t,k,c) )
-                        sys.exit(-1)
+    
+                    # 時間差に関する確率
+                    for l in range(len(self.lag_range)):
+                        lag = self.lag_range[l]
+                        
+                        if t+lag<0 or t+lag>=T:
+                            break
+                        
+                        oc = oppnect_classes[t+lag]
+                        a[t,k,c,l] = foward_prob * self.interrel_prob[oc,c] * self.lag_probs[l]
+                        if math.isnan(foward_prob):
+                            print( "a[t=%d,k=%d,c=%d] became NAN!!" % (t,k,c) )
+                            sys.exit(-1)
             # 正規化
             if t-self.MIN_LEN>=0:
-                z[t] = np.sum( a[t,:,:] )
-                a[t,:,:] /= z[t]
+                z[t] = np.sum( a[t,:,:,:] )
+                a[t,:,:,:] /= z[t]
                  
         return a
 
@@ -192,30 +232,44 @@ class GPSegmentation():
                 return i
 
 
-    def backward_sampling(self, a, d):
+    def backward_sampling(self, a, d, use_max):
         T = a.shape[0]
         t = T-1
+        L = len(self.lag_range)
 
         segm = []
         segm_class = []
+        t_lag_pairs = []
 
         while True:
-            idx = self.sample_idx( a[t].reshape( self.MAX_LEN*self.numclass ))
+            if not use_max:
+                # サンプリングする                
+                idx = self.sample_idx( a[t].reshape( self.MAX_LEN*self.numclass*L ))
+            else:                
+                # サンプリングはせずに確率の最大値を使う
+                idx = np.argmax( a[t].reshape( self.MAX_LEN*self.numclass*L ) )
 
-            k = int(idx/self.numclass)
-            c = idx % self.numclass
+            #k = int(idx/self.numclass)
+            #c = idx % self.numclass
+            
+            
+            k = int(idx/(self.numclass*L))
+            c = int((idx % (self.numclass*L))/L)
+            l = (idx % (self.numclass*L)) % L
+                        
 
             s = d[t-k:t+1]
 
             segm.insert( 0, s )
             segm_class.insert( 0, c )
+            t_lag_pairs.append( (t, self.lag_range[l]) )
 
             t = t-k-1
 
             if t<=0:
                 break
 
-        return segm, segm_class
+        return segm, segm_class, t_lag_pairs
 
     def calc_trans_prob( self ):
         self.trans_prob = np.zeros( (self.numclass,self.numclass) )
@@ -235,6 +289,20 @@ class GPSegmentation():
         # 正規化
         self.trans_prob = self.trans_prob / self.trans_prob.sum(1).reshape(self.numclass,1)
 
+    # excluded_idxを除いた残りで，相互関係確率を計算
+    def calc_interrel_prob(self, excluded_idx ):
+        trans_count = np.zeros( (self.numclass, self.numclass) ) + 0.1
+        own_classes = self.get_classes_list()
+        opponent_classes = self.opponent_gp.get_classes_list()
+        for i in range(len(self.data)):
+            if i!=excluded_idx:
+                for t, lag in self.t_lag_pairs[i]:
+                    c1 = opponent_classes[i][t+lag]
+                    c2 = own_classes[i][t]
+                    trans_count[c1,c2] += 1
+
+        self.interrel_prob = trans_count / trans_count.sum(1).reshape(self.numclass,1)
+                    
 
     # list.remove( elem )だとValueErrorになる
     def remove_ndarray(self, lst, elem ):
@@ -247,7 +315,11 @@ class GPSegmentation():
                 return
         raise ValueError( "ndarray is not found!!" )
 
-    def learn(self):
+    def learn(self, opponent_gp, use_max=False ):
+        # use_max: サンプリングをせずに，最大値を選択
+        
+        # 相手のクラス
+        self.opponent_gp = opponent_gp
         if self.is_initialized==False:
             # GPの学習
             for i in range(len(self.segments)):
@@ -261,12 +333,12 @@ class GPSegmentation():
 
             self.is_initialized = True
 
-        self.update(True)
+        self.update(True, use_max)
 
     def recog(self):
         self.update(False)
 
-    def update(self, learning_phase=True ):
+    def update(self, learning_phase=True, use_max=False ):
 
         for i in range(len(self.segments)):
             d = self.data[i]
@@ -287,13 +359,14 @@ class GPSegmentation():
 
                 # 遷移確率更新
                 self.calc_trans_prob()
+                self.calc_interrel_prob(i)
 
             start = time.clock()
             print( "forward...", end="")
-            a = self.forward_filtering( d )
+            a = self.forward_filtering( i )
 
             print( "backward...", end="" )
-            segm, segm_class = self.backward_sampling( a, d )
+            segm, segm_class, t_lag_pairs = self.backward_sampling( a, d, use_max )
             print( time.clock()-start, "sec" )
 
             print( "Number of classified segments: [", end="")
@@ -303,6 +376,7 @@ class GPSegmentation():
 
 
             self.segments[i] = segm
+            self.t_lag_pairs[i] = t_lag_pairs
 
             for s,c in zip( segm, segm_class ):
                 self.segmclass[id(s)] = c
@@ -318,7 +392,7 @@ class GPSegmentation():
 
                 # 遷移確率更新
                 self.calc_trans_prob()
-
+                self.calc_interrel_prob(-1)
 
 
         return
@@ -332,4 +406,17 @@ class GPSegmentation():
                 lik += self.gps[c].calc_lik( np.arange(len(s), dtype=np.float) , s )
 
         return lik
+
+
+    # クラスを一列に並べた配列を返す
+    def get_classes_list(self):
+        classes_list = []
+        for n,segm in enumerate(self.segments):
+            classes = []
+            for s in segm:
+                if id(s) in self.segmclass:
+                    c = self.segmclass[id(s)]
+                    classes += [ c for i in range(len(s)) ]
+            classes_list.append( classes )
+        return classes_list
 
--- main.py
+++ main.py
@@ -1,45 +1,78 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from __future__ import print_function
-from GPSegmentation import GPSegmentation
+from CGPSegmentation import CGPSegmentation
 import time
+import os
+import numpy as np
+import matplotlib.pyplot as plt
 
-def learn( savedir ):
-    gpsegm = GPSegmentation(2,5)
+def plot_result( savedir, ndata ):
+    for n in range(ndata):
+        classes_a = np.loadtxt( os.path.join( savedir, "A", "segm%03d.txt"%n ) )[:,0]
+        classes_b = np.loadtxt( os.path.join( savedir, "B", "segm%03d.txt"%n ) )[:,0]
+        lags = np.loadtxt( os.path.join( savedir, "A", "lag%03d.txt"%n ) )
+    
+        plt.figure()
+        plt.plot( range(len(classes_a)), classes_a, "o" )
+        plt.plot( range(len(classes_b)), classes_b, "x" )
+        
+        for t in range(len(classes_a)):
+            lag = int(lags[t])
+            
+            if lag!=0:
+                c1 = classes_a[t]
+                c2 = classes_b[t+lag]                
+                plt.plot( [t,t+lag], [c1,c2], "k-" )
+        
+        plt.savefig( os.path.join( savedir, "result%03d.png" % n ) )
+        
+    #plt.show()
+        
 
-    files =  [ "testdata2d_%03d.txt" % j for j in range(4) ]
-    gpsegm.load_data( files )
+def learn_cgphsmm( savedir ):
+    
+    if not os.path.exists(savedir):
+        os.mkdir( savedir )
+    
+    gp_a = CGPSegmentation(2,5)
+    gp_b = CGPSegmentation(2,5)
+
+    files_a =  [ "a%03d.txt" % j for j in range(8) ]
+    files_b =  [ "b%03d.txt" % j for j in range(8) ]
+    gp_a.load_data( files_a )
+    gp_b.load_data( files_b )
 
     start = time.clock()
     for it in range(5):
-        print( "-----", it, "-----" )
-        gpsegm.learn()
-        gpsegm.save_model( savedir )
-        print( "lik =", gpsegm.calc_lik() )
+        # 最後だけ確率の最大値を使う
+        if it==4:
+            use_max=True
+        else:
+            use_max=False
+            
+        print( "*****", it, "*****" )
+
+        # Aの学習
+        print( "--- A" )
+        gp_a.learn( gp_b, use_max )
+        gp_a.save_model( os.path.join(savedir, "A/") )
+        print( "lik", gp_a.calc_lik() )
+
+        # Bの学習        
+        print( "--- B" )
+        gp_b.learn( gp_a, use_max )
+        gp_b.save_model( os.path.join(savedir, "B/") )
+        print( "lik", gp_b.calc_lik() )
+    
+        
     print( time.clock()-start )
-    return gpsegm.calc_lik()
-
-
-def recog( modeldir, savedir ):
-    gpsegm = GPSegmentation(2,5)
-
-    gpsegm.load_data( [ "testdata2d_%03d.txt" % j for j in range(4) ] )
-    gpsegm.load_model( modeldir )
-
-
-    start = time.clock()
-    for it in range(5):
-        print( "-----", it, "-----" )
-        gpsegm.recog()
-        print( "lik =", gpsegm.calc_lik() )
-    print( time.clock()-start )
-    gpsegm.save_model( savedir )
 
 
 
 def main():
-    learn( "learn/" )
-    recog( "learn/" , "recog/" )
+    learn_cgphsmm( "learn/" )
+    plot_result( "learn", 8 )
     return
 
 if __name__=="__main__":
